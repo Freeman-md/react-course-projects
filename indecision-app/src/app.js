79d52ced351a78540355e6b13069fc6a7e7b48e8
Freeman-md <freemanmadudili@gmail.com>
2021-07-09 08:44:31 +0100

    Lifecycle Methods

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -9,6 +9,15 @@ class IndecisionApp extends React.Component {
       options: props.options
     }
   }
+  componentDidMount() {
+    console.log('Fetching Data')
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log('Saving Data')
+  }
+  componentWillUnmount() {
+    console.log('componentWillUnmount')
+  }
   handleDeleteOptions() {
     this.setState(() => ({ options: [] }))
   }
