1912fd199bed8f539c5748003352af8c88585c06
Freeman-md <freemanmadudili@gmail.com>
2021-07-17 20:49:10 +0100

    Big button & options list

--- indecision-app/src/components/AddOption.js
+++ indecision-app/src/components/AddOption.js
@@ -21,7 +21,7 @@ export default class AddOption extends React.Component {
       {this.state.error && <p>{this.state.error}</p>}
       <form onSubmit={this.handleAddOption}>
         <input type="text" name="option" />
-        <button type="submit">Submit</button>
+        <button type="submit" className="button">Add Option</button>
       </form>
       </div>
     )
