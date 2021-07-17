1353231107d19c675fcd84c228065e3c0478d378
Freeman-md <freemanmadudili@gmail.com>
2021-07-17 19:33:50 +0100

    Setting up react-modal

--- indecision-app/src/components/IndecisionApp.js
+++ indecision-app/src/components/IndecisionApp.js
@@ -3,10 +3,12 @@ import AddOption from './AddOption'
 import Header from './Header'
 import Action from './Action'
 import Options from './Options'
+import OptionModal from './OptionModal'
 
 export default class IndecisionApp extends React.Component {
   state = {
-    options: []
+    options: [],
+    selectedOption: undefined
   }
   handleDeleteOptions = () => {
     this.setState(() => ({ options: [] }))
@@ -21,7 +23,9 @@ export default class IndecisionApp extends React.Component {
   handlePick = () => {
     const randomNum = Math.floor(Math.random() * this.state.options.length)
     const option = this.state.options[randomNum]
-    alert(option)
+    this.setState(() => ({
+      selectedOption: option
+    }))
   }
   handleAddOption = (option) => {
     if (!option) {
@@ -34,6 +38,9 @@ export default class IndecisionApp extends React.Component {
       options: prevState.options.concat(option)
     }))
   }
+  handleClearSelectedOption = () => this.setState(() => ({
+    selectedOption: undefined
+  }))
   componentDidMount() {
     try {
       const json = localStorage.getItem('options')
@@ -72,6 +79,10 @@ export default class IndecisionApp extends React.Component {
         />
         <AddOption 
           handleAddOption={this.handleAddOption}
+        />
+        <OptionModal 
+          selectedOption={this.state.selectedOption}
+          handleClearSelectedOption={this.handleClearSelectedOption}
         />
       </div>
     )
